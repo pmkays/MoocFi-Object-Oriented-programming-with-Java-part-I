e85c3bb2570351ce3429c7f590fd7d7484a4d596
Paula Kurniawan <paula.kurn@gmail.com>
2019-09-03 21:37:59 +1000

    Completed exercise 89

--- week5-089.ClockObject/src/BoundedCounter.java
+++ week5-089.ClockObject/src/BoundedCounter.java
@@ -1,4 +1,49 @@
 
-public class BoundedCounter {
-    // copy here the class BoundedCounter from last weeks assignment 78 
+public class BoundedCounter 
+{
+    private int value;
+    private int upperLimit;
+ 
+    public BoundedCounter(int upperLimit) 
+    {
+        this.upperLimit = upperLimit; 
+        this.value = 0;
+    }
+ 
+    public void next() 
+    {
+        if(this.value < this.upperLimit)
+        {
+            this.value++; 
+        }
+        else
+        {
+            this.value = 0; 
+        }
+    }
+ 
+    public String toString() 
+    {
+        if(value<10)
+        {
+            return "0" + this.value;
+        }
+        else
+        {
+            return "" + this.value;
+        }
+    }
+    
+    public int getValue()
+    {
+        return this.value;
+    }
+    
+    public void setValue(int value)
+    {
+        if (value >= 0 && value <= this.upperLimit)
+        {
+            this.value = value;
+        }
+    }
 }
--- week5-089.ClockObject/src/Main.java
+++ week5-089.ClockObject/src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        Clock clock = new Clock(23, 59, 50);
+        Clock clock = new Clock(23, 59, 58);
 
         int i = 0;
         while (i < 20) {
